fc2e753ef547bba9805497ad9104d752b8c22eeb
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-04-12 19:45:06 +0530

    delete-old-logs

--- 14-delete-old-logs.sh
+++ 14-delete-old-logs.sh
@@ -10,6 +10,6 @@ if [ ! -d $DIRECTORY ] # ! - denotes not exist
 then 
     echo " ERROR: The Source Directory : $DIRECTORY is not found"
     exit 1
-# else 
-    # echo "Source Directory : $DIRECTORY exist"
+else 
+    echo "Source Directory : $DIRECTORY exist"
 fi
