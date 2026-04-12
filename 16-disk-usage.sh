c31e46059e21daf66ecb5824eb7a383e4a8ae134
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-04-12 21:04:53 +0530

    disk-usage

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash 
 
 DISK_USAGE=$(df -hT | grep -vE 'tmp|File|efi')
-THRESHOLD=25% 
+THRESHOLD=25
 MESSSAGE=""
 
 # If disk usage is more than the threshold then 
 
 while IFS= read -r line 
 do 
-    USAGE=$(df -hT | grep -vE 'tmp|File|efi' | awk '{print $6F}')
+    USAGE=$(df -hT | grep -vE 'tmp|File|efi' | awk '{print $6F}' | cut -d % -f1)
     VOLUME=$(df -hT | grep -vE 'tmp|File|efi' | awk '{print $1F}')
     
     if [ $USAGE -gt $THRESHOLD ]
