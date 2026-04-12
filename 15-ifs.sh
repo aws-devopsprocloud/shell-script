b3397bf0b872f85ca3bf25b1bf5b324e8077f208
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-04-12 20:29:54 +0530

    ifs

--- 15-ifs.sh
+++ 15-ifs.sh
@@ -7,10 +7,11 @@ then
     echo "ERROR: The file does not exist"
 else 
     echo "reading the file"
-    while IFS=":" read -r f1 f2 f3 f4 f5 f6
+    while IFS=":" read -r f1 f2 f3 f4 f5 f6 f7
     do 
         echo "Username : $f1"
-        echo "User ID: $f3"
-        echo "Home Directory: $f6"
+        echo "User ID : $f3"
+        echo "Home Directory : $f6"
+        echo "Shell : $f7"
     done < $FILE # use single < to insert the files; triple <<< to insert folder/directory;
 fi
