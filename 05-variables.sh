a6ad445911c364da28392737a0abc3c1bf968c23
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-04-12 08:26:10 +0530

    variables

---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter your username: "
+
+read -s USERNAME 
+
+#Here USERNAME is the variable name, It can be any word where we can store the input from the user
+
+echo "Your username is $USERNAME"
