f754e13a4662b3fd17108881e8b08cd41924c98f
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-04-12 19:40:56 +0530

    delete-old-logs

--- 14-delete-old-logs.sh
+++ 14-delete-old-logs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 
-SOURCE_DIR=$1
 FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14)
 
 
 echo "Please enter the source Directory: "
+read DIRECTORY
 
-if [ ! -d $SOURCE_DIR ] # ! - denotes not exist
+if [ ! -d $DIRECTORY ] # ! - denotes not exist
 then 
-    echo " ERROR: The Source Directory : $SOURCE_DIR is not found"
+    echo " ERROR: The Source Directory : $DIRECTORY is not found"
 else 
