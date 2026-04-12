5e7aa0de80fd4a04ff896f8ea53d779c509869a2
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-04-12 08:25:15 +0530

    hello-world

---
+++ 02-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PERSON1=Suresh
+PERSON2=Prem
+
+# echo "Suresh: Hello Ramesh, How are you?"
+# echo "Ramesh: Hi Suresh, I'm good. How are you doing ?"
+# echo "Suresh: I'm doing good Ramesh, Are you coming to Office today?"
+# echo "Ramesh: Yes, I am. Can I come with you Suresh?"
+# echo "Suresh: Absolutely, No problem."
+
+echo "$PERSON1: Hello $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I'm good. How are you doing ?"
+echo "$PERSON1: I'm doing good $PERSON2, Are you coming to Office today?"
+echo "$PERSON2: Yes, I am. Can I come with you $PERSON1? as my bike is still with mechanic for a repair"
+echo "$PERSON1: Absolutely, No problem."
