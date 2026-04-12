a695f722f234a09f6b92cf63126055308cb41381
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-04-12 08:25:56 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Now we can use the commandline arguments 
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I'm good. How are you doing ?"
+echo "$PERSON1: I'm doing good $PERSON2, Are you coming to Office today?"
+echo "$PERSON2: Yes, I am. Can I come with you $PERSON1? as my bike is still with mechanic for a repair"
+echo "$PERSON1: Absolutely, No problem."
