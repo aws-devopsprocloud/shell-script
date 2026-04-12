d4e1e5d1f74ba5f1af83fbbb600f8751e91a0e2b
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-04-12 09:24:09 +0530

    conditions

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
 ID=$(id -u)
+DATE=$(date +%H-%M-%S)
+LOGFILE=/tmp/$0-$DATE.log
+
+VALIDATE () {
+    if [ $? -ne 0 ]
+        then
+            echo "ERROR: Installing MySQL is FAILED"
+        else 
+            echo "Installing MySQL is SUCCESS"
+        fi
+}
 
 if [ $ID -ne 0 ]
 then 
     echo "ERROR: Please run this command with Root permissions"
+    exit 1
 else 
-    yum list installed | grep mysql 
+    yum list installed | grep mysql &>> $LOGFILE
     if [ $? -ne 0 ]
     then 
-        yum install mysql -y 
+        yum install mysql -y &>> $LOGFILE
+        VALIDATE 
+        
     else 
         echo "MySQL is already installed"
     fi
