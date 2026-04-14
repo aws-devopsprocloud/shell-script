4cfb6ab6456ba4d2d952e2c7dcb3f72bf2b1ea48
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-04-14 10:00:47 +0530

    mail

--- mail.sh
+++ mail.sh
@@ -1,13 +1,20 @@
-#!/bin/bash
+# To Address
+# Subject
+# Body Message
+# Alert Type
+# IP Address
+# To Team
+
 TO_ADDRESS=$1
 SUBJECT=$2
 MESSAGE_BODY=$3
 ALERT_TYPE=$4
-SERVER_IP=$5
+IP_ADDRESS=$5
 TO_TEAM=$6
-FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
 
-FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" template.html)
+FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g') # Removing the special characters in the Message
+
+FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$T$IP_ADDRESS/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" template.html)
 
 {
 echo "To: $TO_ADDRESS"
