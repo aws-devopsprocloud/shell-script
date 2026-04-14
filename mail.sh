7188530c62345a4830cf5771f5a9065be3817d3c
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-04-14 20:46:27 +0530

    mail

--- mail.sh
+++ mail.sh
@@ -1,20 +1,23 @@
 # To Address
 # Subject
+# To Team
 # Body Message
 # Alert Type
 # IP Address
-# To Team
+
 
 TO_ADDRESS=$1
 SUBJECT=$2
-MESSAGE_BODY=$3
-ALERT_TYPE=$4
-IP_ADDRESS=$5
-TO_TEAM=$6
+TO_TEAM=$3
+MESSAGE_BODY=$4
+ALERT_TYPE=$5
+IP_ADDRESS=$6
+
 
 FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g') # Removing the special characters in the Message
 
-FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$T$IP_ADDRESS/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" template.html)
+FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$T$IP_ADDRESS/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" template.html) #Replacing the place holders in email template
+
 
 {
 echo "To: $TO_ADDRESS"
