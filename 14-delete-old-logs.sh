f5e826ac188b758acd0c0b97999cec15d88c705c
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-04-14 20:46:48 +0530

    delete-old-logs

--- 14-delete-old-logs.sh
+++ 14-delete-old-logs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
 
-
-
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 echo "Please enter the source Directory: "
 read DIRECTORY
@@ -15,7 +14,15 @@ else
     FILES_TO_DELETE=$(find $DIRECTORY -type f -mtime +14 -name "*.log")
     while IFS= read -r line 
     do 
-        echo "Deleting : $line"
+        echo "Deleted : $line"
         rm -rf $line
     done <<< $FILES_TO_DELETE # use single < to insert the files; triple <<< to insert folder/directory;
 fi
+
+sh mail.sh "premsagar.eri@devopsprocloud.in" "Old Logs deletion on $IP_ADDRESS" "DevOps team" "$line" "Old Logs Deletion" "$IP_ADDRESS"
+# To Address
+# Subject
+# To Team
+# Body Message
+# Alert Type
+# IP Address
